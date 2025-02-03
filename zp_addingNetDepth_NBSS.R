b9358abd8eafbbb1374465e52b1fd5009dd64fa0
Alexandra Cabanelas <acabanelas@whoi.edu>
2025-02-03 17:35:32 -0500

    the biomass file didnt have net max depth so i added it from the inventory metadata file

---
+++ zp_addingNetDepth_NBSS.R
@@ -0,0 +1,57 @@
+################################################################################
+#############          Pelagic Synthesis           #############################
+#############             AUG-2024                 #############################
+#############           Size Spectra               #############################
+## by: Alexandra Cabanelas 
+################################################################################
+
+# just adding net max depth column to the 
+# zp_NESLTER_10m2_ESD_11DEC24 csv
+
+## ------------------------------------------ ##
+#            Packages -----
+## ------------------------------------------ ##
+library(dplyr)
+
+## ------------------------------------------ ##
+#            Data -----
+## ------------------------------------------ ##
+metadata <- read.csv(file.path("raw",
+                               "nes-lter-zooplankton-tow-metadata-v2.csv"),
+                     header = T) #from zooplankton inventory package
+
+zp <- read.csv(file.path("output",
+                         "zp_NESLTER_10m2_ESD_11DEC24.csv"),
+               header = T)
+
+
+## add sample name column to zp
+zp <- zp %>%
+  mutate(sample_name = paste(cruise, station, paste0("B", cast), sep = "_"))
+
+
+## add net depth 
+zp <- zp %>%
+  left_join(metadata %>% 
+              select(sample_name, net_max_depth_m), 
+            by = "sample_name")
+
+
+
+zp_na <- zp %>% filter(is.na(net_max_depth_m))
+
+#due to a typo in the metadata df that i wont fix right now i need to manually assign
+
+## 121 m AR37 L7 B9 (sample name is wrong in the metadata file)
+
+## 231.05 m Lu11c B29
+
+zp <- zp %>%
+  mutate(net_max_depth_m = case_when(
+    sample_name == "EN627_u11c_B29" ~ 231.05,
+    sample_name == "AR38_L7_B9" ~ 121,
+    TRUE ~ net_max_depth_m  # Keep existing values
+  ))
+
+
+write.csv(zp, "output/zp_NESLTER_10m2_ESD_11DEC24.csv")
